09df65ad54b5c28074eaba3fe2f58b9865bf4b7e
JakuBB96 <JakuBB96@javadev.com>
2021-06-25 14:15:49 +0200

    initial commit

---
+++ src/main/java/com/barancewicz/wearfortheweather/WearfortheweatherApplication.java
@@ -0,0 +1,13 @@
+package com.barancewicz.wearfortheweather;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WearfortheweatherApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WearfortheweatherApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/barancewicz/wearfortheweather/WearfortheweatherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.barancewicz.wearfortheweather;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WearfortheweatherApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
